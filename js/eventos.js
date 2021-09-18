e4af34f309270994fee09c423f4565ffc0bde90d
Phytion1318 <danielvieiraperei@gmail.com>
2021-09-18 09:50:18 -0300

    criou const pros elementos do html

---
+++ js/eventos.js
@@ -0,0 +1,4 @@
+const BTN_CADASTRAR = document.getElementById('btn-cadastrar');
+const BTN_LISTAR = document.getElementById('btn-listar');
+const SECTION_CADASTRAR = document.getElementById('section-cadastrar');
+const SECTION_LISTAR = document.getElementById('section-listar');
