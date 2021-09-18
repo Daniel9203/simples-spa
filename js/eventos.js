488980e13ca757193858eb7eaca1dcdf92e79fe7
Phytion1318 <danielvieiraperei@gmail.com>
2021-09-18 10:14:55 -0300

    ocultar e aparecer

--- js/eventos.js
+++ js/eventos.js
@@ -2,3 +2,13 @@ const BTN_CADASTRAR = document.getElementById('btn-cadastrar');
 const BTN_LISTAR = document.getElementById('btn-listar');
 const SECTION_CADASTRAR = document.getElementById('section-cadastrar');
 const SECTION_LISTAR = document.getElementById('section-listar');
+
+BTN_LISTAR.addEventListener('click', function () {
+  SECTION_CADASTRAR.style.display='none';
+  SECTION_LISTAR.style.display='block';
+});
+
+BTN_CADASTRAR.addEventListener('click', function () {
+  SECTION_CADASTRAR.style.display = 'block';
+  SECTION_LISTAR.style.display = 'none';
+});
